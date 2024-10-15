00d8c1a46d270a3f2b6f889107b0a27a3b758d32
Desmond Battle <desmondbattle@Desmonds-MacBook-Air.local>
2024-10-15 18:21:32 -0400

    frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+import { restoreCSRF,csrfFetch } from './store/csrf';
+const store = configureStore();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
--- frontend/src/pages/LoginFormPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginFormPage = () => {
-  return (
-    <div>LoginFormPage</div>
-  )
-}
-
-export default LoginFormPage
